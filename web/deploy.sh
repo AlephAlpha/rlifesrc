db764f1a977f0af4809e7a38af24ac9977c7aa83
AlephAlpha <alephalpha911@gmail.com>
2019-10-02 00:04:46 +0800

    :bug: 修正 deploy.sh 中的错误

--- web/deploy.sh
+++ web/deploy.sh
@@ -11,13 +11,19 @@ echo "Copying files..."
 cp ../target/wasm32-unknown-unknown/release/*.{js,wasm} .deploy_git/
 cp -r static/* .deploy_git/
 
-if [ $1 != "-c" ]; then
-    echo "Deploying..."
-    cd .deploy_git
-    git add -A
-    git commit -m "网页版更新：$(date)"
-    git push origin HEAD:gh-pages
-fi
+while getopts ":d" o; do
+    case "${o}" in
+    d)
+        echo "Deploying..."
+        cd .deploy_git
+        git add -A
+        git commit -m "网页版更新：$(date)"
+        git push origin HEAD:gh-pages
+        ;;
+    *) ;;
+    esac
+done
+shift $((OPTIND - 1))
 
 cd $here
 echo "Done!"
