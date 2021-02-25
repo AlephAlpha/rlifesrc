2d854d47cd0182828e7972b87bca47d4715f84e0
AlephAlpha <alephalpha911@gmail.com>
2021-02-25 21:41:04 +0800

    :bug: 啊……

--- lib/src/world.rs
+++ lib/src/world.rs
@@ -73,8 +73,7 @@ impl<'a, R: Rule> World<'a, R, ReasonNoBackjump> {
         let size = ((config.width + 2) * (config.height + 2) * config.period) as usize;
         let mut cells = Vec::with_capacity(size);
 
-        let is_front = config
-            .backjump
+        let is_front = (!config.backjump)
             .then(|| config.is_front_fn(rule.has_b0(), &search_order))
             .flatten();
 
